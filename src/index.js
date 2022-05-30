e3612f5ca484b1150fdabf93911e1287fc198593
DatsOlena <datsolena@gmail.com>
2022-05-30 07:36:34 -0400

    feat: add tag sorter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("Hello");
+
+
+window.onload = function () {
+    console.log("Hello");
+
+    // Tags
+    addTagsClickHendler();
+
+}
+
+const addTagsClickHendler = () => {
+    document.querySelector('.strategies__tags').addEventListener('click', (e) => {
+        if (e.target.classList.contains('tag')) {
+            let clickedTag = e.target;
+            removeSelectedTags();
+            selectClickedTag(clickedTag);
+        }
+    })
+
+}
+
+const removeSelectedTags = () => {
+    let tags = document.querySelectorAll('.strategies__tags .tag');
+    tags.forEach(tag => {
+        tag.classList.remove('tag_selected');
+        tag.classList.add('tag_bordered');
+    })
+}
+
+const selectClickedTag = (clickedTag) => {
+    clickedTag.classList.add('tag_selected');
+    clickedTag.classList.remove('tag_bordered');
+}
